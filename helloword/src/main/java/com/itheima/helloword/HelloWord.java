305c7ff9cc393bc5aba39acf045f3a49da645be3
xiaoxuesheng <xiaoxuesheng@com>
2020-03-26 19:33:17 +0800

    删除

--- helloword/src/main/java/com/itheima/helloword/HelloWord.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.helloword;
-
-public class HelloWord {
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("HelloWord第：" + i);
-        }
-    }
-}
