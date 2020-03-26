b75b0e691bb9aa64fcad80fb05df0035d78d5a53
xiaoxuesheng <xiaoxuesheng@com>
2020-03-26 19:31:54 +0800

    提交HelloWord

---
+++ helloword/src/main/java/com/itheima/helloword/HelloWord.java
@@ -0,0 +1,9 @@
+package com.itheima.helloword;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("HelloWord第：" + i);
+        }
+    }
+}
